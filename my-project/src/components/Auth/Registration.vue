2e25a2dad42b24d9e3a4648dd120e6e0247a9507
MaksShadows <Lykanenko1995@gmail.com>
2019-04-06 22:37:01 +0300

    Vue

---
+++ my-project/src/components/Auth/Registration.vue
@@ -0,0 +1,63 @@
+<template>
+	<v-container fluid fill-height>
+		<v-layout align-center justify-center>
+			<v-flex xs12 sm8 md4>
+				<v-card class="elevation-12">
+					<v-toolbar dark color="red">
+						<v-toolbar-title>Registration form</v-toolbar-title>
+
+					</v-toolbar>
+					<v-card-text>
+						<v-form v-model="valid" ref="form" validation>
+							<v-text-field prepend-icon="person" name="email" label="E-mail" type="email" v-model="email" :rules='emailRules'></v-text-field>
+							<v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" :counter='6' v-model="password" :rules='passwordRules'></v-text-field>
+							<v-text-field id="password" prepend-icon="lock" name="Confirm password" label="Confirm password" type="password" :counter='6' v-model="confirmPassword" :rules='confirmPasswordRules'></v-text-field>
+						</v-form>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn color="primary" @click="onSubmit" :disabled='!valid'>Create Account</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+	const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+	export default {
+		data() {
+			return {
+				email: '',
+				password: '',
+				confirmPasswordRules: '',
+				valid: false,
+				emailRules: [
+					v => !!v || 'E-mail is required',
+					v => emailRegex.test(v) || 'E-mail must be valid'
+				],
+				passwordRules: [
+					v => !!v || 'Password is required',
+					v => v.length >= 6 || 'Password must be less than 6 characters'
+				],
+				confirmPasswordRules: [
+					v => !!v || 'Password is required',
+					v => v === this.password || 'Password should match'
+				]
+			}
+		},
+		methods: {
+			onSubmit() {
+				if (this.$refs.form.validate()) {
+					const user = {
+						email: this.email,
+						password: this.password
+					}
+					console.log(user)
+				}
+			}
+		}
+	}
+
+</script>
